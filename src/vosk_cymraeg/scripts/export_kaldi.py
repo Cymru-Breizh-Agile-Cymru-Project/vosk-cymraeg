32a0dceaf8306e5af0e6f01395f07f553fcf7afa
Gweltaz Duval-Guennoc <gweltou@hotmail.com>
2025-01-19 12:42:02 +0100

    first draft for Kaldi export script

---
+++ src/vosk_cymraeg/scripts/export_kaldi.py
@@ -0,0 +1,141 @@
+import argparse
+import os
+from pathlib import Path
+from typing import List
+
+import polars as pl
+from rich.console import Console
+
+
+def main() -> None:
+    """Create a training/test corpus for Kaldi"""
+    args = _get_args()
+    console = Console()
+
+    # Load merged corpora
+    train_dataset = pl.read_csv(args.train)
+    
+    build_text_corpus(args.output)
+    build_lexicon([], args.output)
+    
+    # silence_phones.txt
+    silence_phones_path  = args.output / "local/dict_nosp/silence_phones.txt"
+    with open(silence_phones_path, 'w', encoding='utf-8') as f:
+        f.write(f'SIL\noov\nSPN\nLAU\nNSN\n')
+    
+    # nonsilence_phones.txt
+    nonsilence_phones_path = args.output / "local/dict_nosp/nonsilence_phones.txt"
+    with open(nonsilence_phones_path, 'w', encoding='utf-8') as f:
+        pass
+        # for p in sorted(phonemes):
+        #     f.write(f'{p}\n')
+    
+    # optional_silence.txt
+    optional_silence_path  = args.output / "local/dict_nosp/optional_silence.txt"
+    with open(optional_silence_path, 'w', encoding='utf-8') as f:
+        f.write('SIL\n')
+    
+    # Build files specific to train/test datasets
+    build_dataset(
+        "train",
+        train_dataset,
+        args.train.parent / "clips",
+        args.output
+    )
+    
+
+
+def _get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        "export",
+        description="Script responsible for exporting Kaldi datasets",
+    )
+
+    # Used to wipe the output folder before processing
+    parser.add_argument("--train", help="Path to training dataset csv file", type=Path)
+    parser.add_argument("--test", help="Path to evaluation dataset csv file", type=Path)
+    parser.add_argument("--clear", action="store_true", help="Clears the target folder")
+    parser.add_argument("--output", default="output", help="Target folder for the Kaldi dataset", type=Path)
+
+    # Used to select which datasets to processs. Defaults to all of them
+    # parser.add_argument(
+    #     "--dataset",
+    #     nargs="+",
+    #     choices=list(DATASETS.keys()),
+    #     default=list(DATASETS.keys()),
+    # )
+    return parser.parse_args()
+
+
+def build_text_corpus(output_path: Path) -> None:
+    """Build the text corpus"""
+    pass
+
+
+def build_lexicon(words: List[str], output_path: Path) -> None:
+    """
+        Build a lexicon from a list of words
+        
+            Silences            -> SIL
+            Spoken noises       -> SPN
+            Non-spoken noises   -> NSN
+            Laughter            -> LAU
+    """
+
+    special_tags = {
+        "<anadlu>":                 "SPN",
+        "<anadlu i mewn yn sydyn>": "SPN",
+        "<aneglur>":                "SPN",
+        "<cerddoriaeth>":           "NSN",
+        "<chwerthin>":              "LAU",
+        "<chwibanu>":               "SPN",
+        "<chwythu allan>":          "SPN",
+        "<clapio>":                 "NSN",
+        "<clirio gwddf>":           "SPN",
+        "<cusanu>":                 "SPN",
+        "<distawrwydd>":            "SIL",
+        "<ochneidio>":              "SPN",
+        "<PII>":                    "SPN",
+        "<peswch>":                 "SPN",
+        "<sniffian>":               "SPN",
+        "<twtian>":                 "SPN",
+    }
+
+    output_path = output_path / "local/dict_nosp"
+    os.makedirs(output_path, exist_ok=True)
+
+    with open(output_path / "lexicon.txt", 'w', encoding='utf-8') as _f:
+            # Write special phones
+            _f.write("!SIL SIL\n"
+                        "<UNK> SPN\n")
+            for tag in sorted(special_tags.keys()):
+                 _f.write(f"{tag} {special_tags[tag]}\n")
+            
+            # Write regular words with phonetization
+
+
+def build_dataset(
+        name: str,
+        df: pl.DataFrame,
+        clips_path: Path,
+        output_path: Path
+    ) -> None:
+    print(clips_path)
+    dataset_path = output_path / name
+    os.makedirs(dataset_path, exist_ok=True)
+
+    # Build 'text' file
+    with open(dataset_path / "text", 'w', encoding='utf-8') as _f:
+        for row in df.rows():
+            _f.write(f"{row[1]}\t{row[2]}\n")
+    
+    # Build 'utt2spk'
+    with open(dataset_path / "utt2spk", 'w', encoding='utf-8') as _f:
+        for row in df.sort("utterance").rows():
+            _f.write(f"{row[1]}\t{row[0]}\n")
+    
+    # Build 'wav.scp'
+    with open(dataset_path / "wav.scp", 'w', encoding='utf-8') as _f:
+        for row in df.sort("utterance").rows():
+            audio_path = clips_path / (row[1] + ".wav")
+            _f.write(f"{row[1]}\t{audio_path}\n")
