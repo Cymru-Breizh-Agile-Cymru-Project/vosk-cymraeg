768cd3d461fc16a3b11f14810eb6f5f26be7c121
Preben Vangberg <prv21fgt@bangor.ac.uk>
2025-02-12 12:31:13 +0000

    Added script to combine datasets
    Resolves: #4

---
+++ src/vosk_cymraeg/scripts/combine_datasets.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import polars as pl
+
+
+def main():
+    """Combines all the predefined splits for the various datasets into a single split
+    to create a combined dataset"""
+    output_path = Path("data/processed/dataset")
+    output_path.mkdir(parents=True, exist_ok=True)
+    interim_path = Path("data/interim")
+
+    print(f"Exporting combined dataset to {output_path}")
+
+    def combine_and_write(name: str, files: list[str]) -> None:
+        """Reads the files file polars (paths given relative to the interim folder) and
+        exports them to the output path with the given name"""
+        pl.concat([pl.read_csv(interim_path / file) for file in files]).write_csv(
+            output_path / f"{name}.csv"
+        )
+
+    # Training set
+    combine_and_write(
+        "train", ["banc/train.csv", "cv/cy/train.csv", "lleisiau_arfor/train_clean.csv"]
+    )
+    # Validation set
+    combine_and_write(
+        "dev", ["banc/validation.csv", "cv/cy/dev.csv", "lleisiau_arfor/dev_clean.csv"]
+    )
+    # Test set
+    combine_and_write(
+        "test", ["banc/test.csv", "cv/cy/test.csv", "lleisiau_arfor/test_clean.csv"]
+    )
