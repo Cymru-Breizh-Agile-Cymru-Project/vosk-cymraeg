4b01faf44cdb9f2bf5e57ba0a3f971f9ec1e1d96
Gweltaz Duval-Guennoc <gweltou@hotmail.com>
2025-03-06 20:10:09 +0100

    Use the logger

--- src/vosk_cymraeg/scripts/combine_datasets.py
+++ src/vosk_cymraeg/scripts/combine_datasets.py
@@ -1,20 +1,28 @@
+import logging
 from pathlib import Path
 
 import polars as pl
 
 
+_logger = logging.getLogger(__name__)
+
+
 def main():
     """Combines all the predefined splits for the various datasets into a single split
     to create a combined dataset"""
+    logging.basicConfig(
+        level="INFO", format="%(message)s", datefmt="[%X]"
+    )
+
     output_path = Path("data/processed/dataset")
     output_path.mkdir(parents=True, exist_ok=True)
     interim_path = Path("data/interim")
 
-    print(f"Exporting combined dataset to {output_path}")
+    _logger.info(f"Exporting combined dataset to {output_path}")
 
     def combine_and_write(name: str, files: list[str]) -> None:
-        """Reads the files file polars (paths given relative to the interim folder) and
-        exports them to the output path with the given name"""
+        """Reads the files with polars (paths given relative to the interim folder),
+        and exports the combined dataset to the output path with the given name"""
         pl.concat([pl.read_csv(interim_path / file) for file in files]).write_csv(
             output_path / f"{name}.csv"
         )
